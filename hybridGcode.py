66eb5db86f58b94f06769a70170d3043c95d2a1a
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2026-05-01 20:10:32 -0500

    cleaned up comments and variables

--- hybridGcode.py
+++ hybridGcode.py
@@ -43,19 +43,20 @@ class layer:
         self.lineStart = lineStart
         self.z = z
 
+# processing parameters
+multipass = False
+machiningFeedRate = 300     # machining feed rate (mm/min)
+zHopHeight = 50             # rise between printing and machining processes (mm)
+zHopNozzleRetract = 4       # retraction for z-hopping (mm)
+
 # file reading
 processes = []
 features = []
 layers = []
 
-multipass = False
-
 readSkip = []
 writeSkip = []
 writeInsert = {}
-machiningFeedRate = "F300"
-zHopHeight = "Z-50"
-zHopNozzleRetract = "4"     # 4mm retraction for z-hopping
 
 firstComments = []
 firstMCodes = []
@@ -64,7 +65,7 @@ firstHome = []
 with open(filename, 'r') as inFile:
     lines = [line.strip() for line in inFile]
 
-#prepass remove empty lines
+# prepass remove empty lines
 for lineNum, line in enumerate(lines):
     if len(line) < 1:
         readSkip.append(lineNum)
@@ -103,7 +104,6 @@ for lineNum, line in enumerate(lines):
             layers.append(layer(keywords[2][:-1], lineNum, keywords[5]))
         else:
             layers.append(layer(keywords[2], lineNum, None))
-        # print(lineNum, lines[lineNum])
 
 writeSkip.extend(firstComments)
 writeSkip.extend(firstMCodes)
@@ -123,6 +123,7 @@ for lineNum, line in enumerate(lines):
 
         # replaces E motor with A motor reversed (extruder)
         workingLine = workingLine.replace(" E", " A-")
+
         # flips coordinate system upside down (machines counterclockwise by right hand rule)
         workingLine = workingLine.replace(" Z", " Z-").replace("--", "-")
 
@@ -145,15 +146,17 @@ for lineNum, line in enumerate(lines):
     if currentProcess == processMachining and keywords[0] != ';':
         matches = [keyword for keyword in keywords if re.match("F", keyword)]
         if len(matches) == 1:
-            keywords[keywords.index(matches[0])] = machiningFeedRate
+            keywords[keywords.index(matches[0])] = "F" + str(machiningFeedRate)
             lines[lineNum] = " ".join(keywords)
     if currentProcess == processMachining and currentFeature == "skirt":
         writeSkip.append(lineNum)
 
+
+for i in layers:
+    print(i.lineStart, i.z)
+
 # by process modifications
 for processNum in range(0, len(processes) - 1):
-    # print("process:", processes[processNum].process)
-
     currentProcess = processes[processNum]
     nextProcess = processes[processNum + 1]
 
@@ -161,8 +164,7 @@ for processNum in range(0, len(processes) - 1):
     if currentProcess.process == processMachining:
 
         # insert Z rise before each machining process
-        writeInsert[currentProcess.lineStart - 1] = "G92 A-0\nG1 A" + zHopNozzleRetract + " F2400\n" + "G1 " + zHopHeight + " F1000\n" + "G1 A-" + zHopNozzleRetract + " F2400"
-        # writeInsert[currentProcess.lineStart - 1] = "G92 E-0\nG1 E" + zHopNozzleRetract + " F2400\n" + "G1 " + zHopHeight + " F1000\n" + "G1 E-" + zHopNozzleRetract + " F2400"
+        writeInsert[currentProcess.lineStart - 1] = "G92 E-0\nG1 E" + str(zHopNozzleRetract) + " F2400\n" + "G1 " + "Z-" + str(zHopHeight) + " F1000\n" + "G1 E-" + str(zHopNozzleRetract) + " F2400"
 
         lineStartOffset = 0
         lineEndOffset = -1
@@ -176,34 +178,29 @@ for processNum in range(0, len(processes) - 1):
         # reverses lines using found offset
         lineSwapMin = currentProcess.lineStart + lineStartOffset
         lineSwapMax = nextProcess.lineStart + lineEndOffset
-        # print(lineSwapMin, lineSwapMax)
-        # print(0, math.ceil((lineSwapMax - lineSwapMin) / 2))
         for lineNum in range(0, math.ceil((lineSwapMax - lineSwapMin) / 2)):
             temp = lines[lineSwapMin + lineNum]
             lines[lineSwapMin + lineNum] = lines[lineSwapMax - lineNum]
             lines[lineSwapMax - lineNum] = temp
-            # print("swapped", lineSwapMin + lineNum, lineSwapMax - lineNum)
             if lineSwapMin + lineNum in writeSkip:
                 if lineSwapMax - lineNum in writeSkip:
                     #if both in writeskip, swap
                     skipTemp = lineSwapMin + lineNum
                     writeSkip[writeSkip.index(lineSwapMin + lineNum)] = lineSwapMax - lineNum
                     writeSkip.append(skipTemp)
-                    # print("writeskip swapped", lineSwapMin + lineNum,  lineSwapMax - lineNum)
                 else:
                     #else rewrite only one
                     writeSkip[writeSkip.index(lineSwapMin + lineNum)] = lineSwapMax - lineNum
-                    # print("writeskip replaced", lineSwapMin + lineNum, lineSwapMax - lineNum)
 
         # find highest z in process and reset z for each layer
-
-
         maxZ = None
-        # print(nextProcess.process)
         for layerNum in range(len(layers)-1, 0, -1):
+            print(layers[layerNum].z)
             if layers[layerNum].lineStart > currentProcess.lineStart and (nextProcess.process=="end" or layers[layerNum].lineStart < nextProcess.lineStart):
-                maxZ = layers[layerNum].z
-                print(currentProcess.process, maxZ)
+                if layers[layerNum].z != None:
+                    maxZ = layers[layerNum].z
+                else:
+                    maxZ = layers[-2].z
                 break
 
         # loop through layers, see if layer is in current process, add z to list
@@ -212,6 +209,8 @@ for processNum in range(0, len(processes) - 1):
             if (nextProcess.process != "end" and layers[layerNum].lineStart > currentProcess.lineStart and layers[layerNum].lineStart < nextProcess.lineStart) or (nextProcess.process == "end" and layers[layerNum].lineStart > currentProcess.lineStart):
                 if layers[layerNum].z != None:
                     currentProcessZList.append(layers[layerNum].z)
+                else:
+                    currentProcessZList.append(layers[-2].z)
         print(currentProcessZList)
 
         # loop through lines of process
@@ -222,49 +221,12 @@ for processNum in range(0, len(processes) - 1):
             
             # if new layer found, turn on writing
             if len(keywords) > 2 and keywords[0] == ';' and keywords[1] == "layer":
+                print(lines[lineNum])
                 writingZ = True
                 writingIndex += 1
             if writingZ and len(keywords) >= 4 and keywords[0] == "G1" and keywords[1][0] == 'X' and keywords[2][0] == 'Y':
                     writeInsert[lineNum] = "G1 Z-" + currentProcessZList[writingIndex]
                     writingZ = False
-
-
-        # for lineNum in range(nextProcess.lineStart, currentProcess.lineStart, -1):
-            
-            # print(lines[lineNum])
-
-        # if multipass:
-        #     # loop through process and add Z reset and Z revert
-        #     writingZ = True
-        #     maxZ = None
-        #     nextZ = None
-
-        #     for lineNum in range(nextProcess.lineStart, currentProcess.lineStart, -1):
-        #         keywords = lines[lineNum].split(' ')
-        #         if len(keywords) > 5 and keywords[0] == ';' and keywords[1] == "layer":
-        #             maxZ = nextZ = float(keywords[5])
-        #             break
-
-        #     for lineNum in range(currentProcess.lineStart + 2, nextProcess.lineStart):
-        #         keywords = lines[lineNum].split(' ')
-
-        #         # change nextZ at every layer comment
-        #         if len(keywords) > 2 and keywords[0] == ';' and keywords[1] == "layer":
-        #             if nextZ == None :
-        #                 nextZ = -50
-        #                 writingZ=True
-        #             else:
-        #                 nextZ -= 1
-        #                 writingZ = True
-                    
-                
-
-        #         # check if first x y line in layer
-        #         if writingZ and isinstance(nextZ, float) and len(keywords) >= 4 and keywords[0] == "G1" and keywords[1][0] == 'X' and keywords[2][0] == 'Y':
-        #             writeInsert[lineNum] = "G1 Z-" + str(nextZ)
-        #             writingZ = False
-
-        #     writeInsert[nextProcess.lineStart - 1] = "G1 Z-" + str(maxZ) + " ; bob"
 
 # write file
 with open(filename.split('.')[0]+"_hybrid."+filename.split('.')[1], 'w') as outFile:
@@ -276,6 +238,6 @@ with open(filename.split('.')[0]+"_hybrid."+filename.split('.')[1], 'w') as outF
                 outFile.write(writeInsert[lineNum] + "\n")
         
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    outFile.write("; G-Code Hybridized by Masaki Maruo\n; " + timestamp)
+    outFile.write("Hybridized " + timestamp)
 
 print(ANSI.OKGREEN + "   File " + filename.split('.')[0]+"_hybrid."+filename.split('.')[1] + " written." + ANSI.ENDC)
